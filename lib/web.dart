6e0e82278b2029df543a06f3edb472c0b38b360d
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-05-19 00:30:07 +0300

    Update web.dart

--- lib/web.dart
+++ lib/web.dart
@@ -24,7 +24,7 @@ class _WebViewClassState extends State<WebViewClass> {
       body: SafeArea(
         child: WebView(
 
-              initialUrl: 'https://my.app2apk.xyz/',
+              initialUrl: 'https://app.rogenso.com.tr/2023/05/login-to-your-account/',
               onWebViewCreated: (WebViewController webViewController) {
                 _controller.complete(webViewController);
               },
