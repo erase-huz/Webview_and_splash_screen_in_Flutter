8efb4d56322b47e3a5445904619fb1f35ea8dcec
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-11-12 15:03:46 +0300

    Update web.dart

--- lib/web.dart
+++ lib/web.dart
@@ -24,7 +24,7 @@ class _WebViewClassState extends State<WebViewClass> {
       body: SafeArea(
         child: WebView(
 
-              initialUrl: 'http://www.doviz.habertanzim.com',
+              initialUrl: 'https://gascom.com.tr',
               onWebViewCreated: (WebViewController webViewController) {
                 _controller.complete(webViewController);
               },
